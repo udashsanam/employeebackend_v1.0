b6cb0f975a4564fddccd89f4275bc99ce28ec544
sanam udash <udashsanam@gmail.com>
2022-01-07 19:37:50 +0545

    add : project created

---
+++ employeebackend/src/main/java/com/web/employeebackend/EmployeebackendApplication.java
@@ -0,0 +1,13 @@
+package com.web.employeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeebackendApplication.class, args);
+	}
+
+}
---
+++ employeebackend/src/test/java/com/web/employeebackend/EmployeebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.employeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
